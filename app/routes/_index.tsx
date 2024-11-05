15f15030fbf15a1609972fbf961cffa0c8819688
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-11-05 16:09:39 +0000

    move the loader to bob route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,22 +7,10 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-import { client } from "../utils/db.server";
-import { useLoaderData } from "@remix-run/react";
-
 export default function Index() {
-  const movies = useLoaderData();
-  console.log(movies);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h3 className="text-2xl text-center">Welcome to the Home page</h3>
     </div>
   );
-}
-
-export async function loader() {
-  let db = await client.db("sample_mflix");
-  let collection = await db.collection("movies");
-  let movies = await collection.find({}).limit(10).toArray();
-  return json(movies);
 }
--- app/routes/bob.jsx
+++ app/routes/bob.jsx
@@ -1,3 +1,17 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+import { client } from "../utils/db.server";
+
 export default function Bob() {
+  const movies = useLoaderData();
+  console.log(movies);
   return <h3 className="text-2xl text-center">Welcome to the Bob page</h3>;
+}
+
+export async function loader() {
+  let db = await client.db("sample_mflix");
+  let collection = await db.collection("movies");
+  let movies = await collection.find({}).limit(10).toArray();
+  return json(movies);
 }
