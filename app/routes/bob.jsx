509b0ac267bc60ee9f00c647548abc20b5e22db0
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-11-05 16:12:35 +0000

    removed json to try and get around unexpected error

--- app/routes/bob.jsx
+++ app/routes/bob.jsx
@@ -1,4 +1,4 @@
-import { json } from "@remix-run/node";
+// import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
 import { client } from "../utils/db.server";
@@ -13,5 +13,5 @@ export async function loader() {
   let db = await client.db("sample_mflix");
   let collection = await db.collection("movies");
   let movies = await collection.find({}).limit(10).toArray();
-  return json(movies);
+  return movies;
 }
