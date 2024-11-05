9b6ad98764a3d0c7c279810c1c50ae0fd117ef03
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-11-05 15:39:48 +0000

    removing loader from home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,8 +11,8 @@ import { client } from "../utils/db.server";
 import { useLoaderData } from "@remix-run/react";
 
 export default function Index() {
-  const movies = useLoaderData();
-  console.log(movies);
+  // const movies = useLoaderData();
+  // console.log(movies);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h3 className="text-2xl text-center">Welcome to the Home page</h3>
@@ -20,9 +20,9 @@ export default function Index() {
   );
 }
 
-export async function loader() {
-  let db = await client.db("sample_mflix");
-  let collection = await db.collection("movies");
-  let movies = await collection.find({}).limit(10).toArray();
-  return json(movies);
-}
+// export async function loader() {
+//   let db = await client.db("sample_mflix");
+//   let collection = await db.collection("movies");
+//   let movies = await collection.find({}).limit(10).toArray();
+//   return json(movies);
+// }
