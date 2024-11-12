fd6c86219432ffffc4b52bb2b22922a3d83c24fb
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-11-12 13:23:07 +0000

    adding new route

---
+++ app/routes/investigation.jsx
@@ -0,0 +1,17 @@
+export default function Investigation() {
+  return (
+    <main>
+      <div className="grid md:grid-cols-2 gap-4">
+        <div className="rounded bg-green-300 p-4 flex justify-center items-center">
+          <h3 className="text-2xl">Hello there</h3>
+        </div>
+        <div className="rounded bg-green-300 p-4 flex justify-center items-center">
+          <h3 className="text-2xl">Hello there</h3>
+        </div>
+        <div className="rounded bg-green-300 p-4 flex justify-center items-center">
+          <h3 className="text-2xl">Hello there</h3>
+        </div>
+      </div>
+    </main>
+  );
+}
