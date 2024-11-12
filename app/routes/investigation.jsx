3bb070f63a205d6bacd7361cfbeb3c01133d03d6
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-11-12 13:29:59 +0000

    three cols on large screen

--- app/routes/investigation.jsx
+++ app/routes/investigation.jsx
@@ -1,7 +1,7 @@
 export default function Investigation() {
   return (
     <main>
-      <div className="grid md:grid-cols-2 gap-4">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div className="rounded bg-green-300 p-4 flex justify-center items-center">
           <h3 className="text-2xl">Hello there</h3>
         </div>
