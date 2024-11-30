552799874cc3d2876f1099c379f8e70fa3b6076e
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-11-30 09:42:21 +0000

    adding kanye quote

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import { json, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,12 +8,33 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+interface Quote {
+  quote: string;
+}
+
 export default function Index() {
+  const quote: Quote = useLoaderData();
+
   return (
     <>
       <main>
-        <section></section>
+        <section className="p-4">
+          <h3 className="text-center text-2xl p-2">As Kanye would say...</h3>
+
+          <div className="grid grid-cols-1">
+            <div className="bg-purple-300 p-4 justify-center items-center text-center">
+              <div>
+                <h5 className="text-2xl">{quote.quote}</h5>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
+}
+
+export async function loader() {
+  const quote = await fetch("https://api.kanye.rest");
+  return quote;
 }
