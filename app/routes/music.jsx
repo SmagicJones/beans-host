f1e25938e1fb8e6125ae55e4b50d1f7fcf94af79
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-11-30 10:05:56 +0000

    starting music blog

--- app/routes/music.jsx
+++ app/routes/music.jsx
@@ -1,3 +1,67 @@
 export default function Music() {
-  return <h1>Music Blog</h1>;
+  return (
+    <main>
+      <section className="p-4">
+        <div className="grid grid-cols-1">
+          <div className="bg-purple-300 p-4 rounded text-center">
+            <h1 className="lg:text-4xl md:text-2xl p-4 ">
+              Bob Makes Music Again
+            </h1>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="grid md:grid-cols-2 gap-4 m-2">
+          <div className="bg-purple-200 rounded p-4">
+            <p className="p-2">
+              So since moving over to working with websites as a full time job I
+              have been reinvigorated with music and can be found most nights
+              playing for a couple of hours.
+            </p>
+            <p className="p-2">
+              I have a number of instruments but there is one in particular
+              which is so easy to pick up and play I feel that I should mention
+              it first.
+            </p>
+            <p className="p-2">
+              She is a little parlor size Gretsch Jim Dandy. I originally wanted
+              it in a lovely electric blue but they didn't have one on the day
+              so I ended up with the one you can see just over in the other
+              column.
+            </p>
+            <p className="p-2">
+              Perhaps I will introduce some more guitars here over time
+            </p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="grid md:grid-cols-2 gap-4 m-2">
+          <div className="bg-purple-200 p-4 rounded">
+            <p className="p-2">
+              So I am finding that I have quite a lot of musical ideas at the
+              moment and because I get bored quickly I have just been playing
+              them on the guitar and not being able to layer any tracks together
+              because me mac had storage space issues.
+            </p>
+            <p className="p-2">
+              However, after sharing this problem with some friends and a trip
+              to the unit I found an even older macintosh that I brought home in
+              the off chance that it would load up and recognise my audio
+              interface - bought at a similar time to the Gretsch.
+            </p>
+            <p className="p-2">
+              As it happened, one good thing after another, the mac decided it
+              could load up and more than that already had a DAW install and
+              recognised the new audio interface.
+            </p>
+            <p className="p-2">
+              Really now I am all out of excuses for making music accept the
+              final one.... I need a new guitar lead!
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
