3994116fdde7ef8d5ad724c77f253155d9f730f7
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-12-09 18:29:58 +0000

    created new play route with new custom tab container

---
+++ app/routes/play.jsx
@@ -0,0 +1,14 @@
+import { NewTab } from "../components/NewTab";
+
+export default function Play() {
+  return (
+    <main>
+      <section>
+        <h1 className="text-center text-8xl">Welcome to play time</h1>
+      </section>
+      <section>
+        <NewTab />
+      </section>
+    </main>
+  );
+}
