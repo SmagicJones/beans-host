2620788462fb6905d0dac30314efa572c66c599d
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-12-18 15:11:50 +0000

    trying mongo connection on mongo route

---
+++ app/routes/mongo.jsx
@@ -0,0 +1,21 @@
+import { useLoaderData } from "@remix-run/react";
+import { client } from "../utils/db.server";
+
+export default function Mongo() {
+  const movie = useLoaderData();
+  console.log(movie);
+  return (
+    <div className="grid md:grid-cols-2">
+      <div className="bg-slate-500">
+        <h3>{movie.title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export async function loader() {
+  let db = await client.db("sample_mflix");
+  let collection = await db.collection("movies");
+  let movie = await collection.findOne();
+  return movie;
+}
