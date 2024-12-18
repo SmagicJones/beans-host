99e44c0b8498677336588885b0bcbf85dca73ebc
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-12-18 16:03:16 +0000

    Try again

--- app/routes/mongo.jsx
+++ app/routes/mongo.jsx
@@ -1,16 +1,18 @@
-import { useLoaderData } from "@remix-run/react";
-import { client } from "../utils/db.server";
-import { json } from "@remix-run/node";
+// import { useLoaderData } from "@remix-run/react";
+// import { client } from "../utils/db.server";
+// import { json } from "@remix-run/node";
 
-export default function Mongo() {
-  //   const movie = useLoaderData();
-  console.log(movie);
-  return (
-    <div className="grid md:grid-cols-2">
-      <div className="bg-slate-500">{/* <h3>{movie.title}</h3> */}</div>
-    </div>
-  );
-}
+// export default function Mongo() {
+//   //   const movie = useLoaderData();
+//   console.log(movie);
+//   return (
+//     <div className="grid md:grid-cols-2">
+//       <div className="bg-slate-500">
+//         <h3>{movie.title}</h3>{" "}
+//       </div>
+//     </div>
+//   );
+// }
 
 // export async function loader() {
 //   let db = await client.db("sample_mflix");
