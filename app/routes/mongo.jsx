de324aabd70e30ca24b66af7c514ea4d15292c47
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-12-18 16:39:52 +0000

    trying mongo in again

--- app/routes/mongo.jsx
+++ app/routes/mongo.jsx
@@ -1,22 +1,22 @@
-// import { useLoaderData } from "@remix-run/react";
-// import { client } from "../utils/db.server";
-// import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { client } from "../utils/db.server";
+import { json } from "@remix-run/node";
 
-// export default function Mongo() {
-//   //   const movie = useLoaderData();
-//   console.log(movie);
-//   return (
-//     <div className="grid md:grid-cols-2">
-//       <div className="bg-slate-500">
-//         <h3>{movie.title}</h3>{" "}
-//       </div>
-//     </div>
-//   );
-// }
+export default function Mongo() {
+  const movie = useLoaderData();
+  console.log(movie);
+  return (
+    <div className="grid md:grid-cols-2">
+      <div className="bg-slate-500">
+        <h3>{movie.title}</h3>{" "}
+      </div>
+    </div>
+  );
+}
 
-// export async function loader() {
-//   let db = await client.db("sample_mflix");
-//   let collection = await db.collection("movies");
-//   let movie = await collection.findOne();
-//   return json(movie);
-// }
+export async function loader() {
+  let db = await client.db("sample_mflix");
+  let collection = await db.collection("movies");
+  let movie = await collection.findOne();
+  return json(movie);
+}
