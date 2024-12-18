aa118d02b911344b12aa9b57624f121e3be322c8
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-12-18 16:01:09 +0000

    taking out the mongo loader to see if it puts the site back

--- app/routes/mongo.jsx
+++ app/routes/mongo.jsx
@@ -3,20 +3,18 @@ import { client } from "../utils/db.server";
 import { json } from "@remix-run/node";
 
 export default function Mongo() {
-  const movie = useLoaderData();
+  //   const movie = useLoaderData();
   console.log(movie);
   return (
     <div className="grid md:grid-cols-2">
-      <div className="bg-slate-500">
-        <h3>{movie.title}</h3>
-      </div>
+      <div className="bg-slate-500">{/* <h3>{movie.title}</h3> */}</div>
     </div>
   );
 }
 
-export async function loader() {
-  let db = await client.db("sample_mflix");
-  let collection = await db.collection("movies");
-  let movie = await collection.findOne();
-  return json(movie);
-}
+// export async function loader() {
+//   let db = await client.db("sample_mflix");
+//   let collection = await db.collection("movies");
+//   let movie = await collection.findOne();
+//   return json(movie);
+// }
