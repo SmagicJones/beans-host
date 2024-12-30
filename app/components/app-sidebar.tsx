c74fcb8938a9439ac800f445d627a88e9c98a038
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2024-12-30 12:17:45 +0000

    adding sidebar

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -1,5 +1,6 @@
-import * as React from "react"
-import { GalleryVerticalEnd } from "lucide-react"
+import * as React from "react";
+import { GalleryVerticalEnd } from "lucide-react";
+import { Link } from "@remix-run/react";
 
 import {
   Sidebar,
@@ -13,148 +14,148 @@ import {
   SidebarMenuSubButton,
   SidebarMenuSubItem,
   SidebarRail,
-} from "~/components/ui/sidebar"
+} from "~/components/ui/sidebar";
 
 // This is sample data.
 const data = {
   navMain: [
     {
-      title: "Getting Started",
+      title: "Blogs",
       url: "#",
       items: [
         {
-          title: "Installation",
-          url: "#",
+          title: "Taste of Turkey",
+          url: "/tasteofturkey",
         },
         {
-          title: "Project Structure",
-          url: "#",
+          title: "Bridgefest",
+          url: "/bridgefest",
         },
       ],
     },
-    {
-      title: "Building Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Routing",
-          url: "#",
-        },
-        {
-          title: "Data Fetching",
-          url: "#",
-          isActive: true,
-        },
-        {
-          title: "Rendering",
-          url: "#",
-        },
-        {
-          title: "Caching",
-          url: "#",
-        },
-        {
-          title: "Styling",
-          url: "#",
-        },
-        {
-          title: "Optimizing",
-          url: "#",
-        },
-        {
-          title: "Configuring",
-          url: "#",
-        },
-        {
-          title: "Testing",
-          url: "#",
-        },
-        {
-          title: "Authentication",
-          url: "#",
-        },
-        {
-          title: "Deploying",
-          url: "#",
-        },
-        {
-          title: "Upgrading",
-          url: "#",
-        },
-        {
-          title: "Examples",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "API Reference",
-      url: "#",
-      items: [
-        {
-          title: "Components",
-          url: "#",
-        },
-        {
-          title: "File Conventions",
-          url: "#",
-        },
-        {
-          title: "Functions",
-          url: "#",
-        },
-        {
-          title: "next.config.js Options",
-          url: "#",
-        },
-        {
-          title: "CLI",
-          url: "#",
-        },
-        {
-          title: "Edge Runtime",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Architecture",
-      url: "#",
-      items: [
-        {
-          title: "Accessibility",
-          url: "#",
-        },
-        {
-          title: "Fast Refresh",
-          url: "#",
-        },
-        {
-          title: "Next.js Compiler",
-          url: "#",
-        },
-        {
-          title: "Supported Browsers",
-          url: "#",
-        },
-        {
-          title: "Turbopack",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Community",
-      url: "#",
-      items: [
-        {
-          title: "Contribution Guide",
-          url: "#",
-        },
-      ],
-    },
+    // {
+    //   title: "Building Your Application",
+    //   url: "#",
+    //   items: [
+    //     {
+    //       title: "Routing",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Data Fetching",
+    //       url: "#",
+    //       isActive: true,
+    //     },
+    //     {
+    //       title: "Rendering",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Caching",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Styling",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Optimizing",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Configuring",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Testing",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Authentication",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Deploying",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Upgrading",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Examples",
+    //       url: "#",
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "API Reference",
+    //   url: "#",
+    //   items: [
+    //     {
+    //       title: "Components",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "File Conventions",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Functions",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "next.config.js Options",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "CLI",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Edge Runtime",
+    //       url: "#",
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "Architecture",
+    //   url: "#",
+    //   items: [
+    //     {
+    //       title: "Accessibility",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Fast Refresh",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Next.js Compiler",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Supported Browsers",
+    //       url: "#",
+    //     },
+    //     {
+    //       title: "Turbopack",
+    //       url: "#",
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "Community",
+    //   url: "#",
+    //   items: [
+    //     {
+    //       title: "Contribution Guide",
+    //       url: "#",
+    //     },
+    //   ],
+    // },
   ],
-}
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -163,15 +164,15 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <a href="#">
+              <Link to="/">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                   <GalleryVerticalEnd className="size-4" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">Documentation</span>
-                  <span className="">v1.0.0</span>
+                  <span className="font-semibold">Boblogs</span>
+                  <span className="">v2.0</span>
                 </div>
-              </a>
+              </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
@@ -204,5 +205,5 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -13,9 +13,28 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
 
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "./components/ui/sidebar";
+
+import { AppSidebar } from "./components/app-sidebar";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "./components/ui/breadcrumb";
+
+import { Separator } from "./components/ui/separator";
+
 import Header from "./components/header";
 
-export default function App() {
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
@@ -24,14 +43,49 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="size-full">
-        <Header />
-        <Outlet />
-
+      <body>
+        {children}
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            {/* <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Bobteaches</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Guitar</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb> */}
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+          <div className="aspect-video rounded-xl bg-muted/50 p-4">Hello</div>
+          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="aspect-video rounded-xl bg-muted/50" />
+        </div> */}
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+            <Outlet />;
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
 
