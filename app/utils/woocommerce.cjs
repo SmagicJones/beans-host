1e0acbca6a7c0d8f4d09d8b1f4d7163fc896dcdc
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2025-02-14 15:41:44 +0000

    security update

--- app/utils/woocommerce.cjs
+++ app/utils/woocommerce.cjs
@@ -5,8 +5,8 @@ const WooCommerceRestApi = pkg.default;
 const wooClient = new WooCommerceRestApi({
   url: "https://devplayground.3dcoded.com/",
 
-  consumerKey: "ck_3e988ace01e50ed338f9c7874ae1caf5cb9ff643",
-  consumerSecret: "cs_cbc53ee16b6c6d4547b47182caf1a9753c417065",
+  consumerKey: `${process.env.CONSUMER_KEY}`,
+  consumerSecret: `${process.env.CONSUMER_SECRET}`,
   version: "wc/v3",
 });
 
